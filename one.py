eec27768709283e71d17f899552a9606432eb722
bfpapa <42157791+bfpapa@users.noreply.github.com>
2018-08-06 19:30:41 -0400

    Update one.py

--- one.py
+++ one.py
@@ -1,2 +1,15 @@
+import os
 import pandas
+
+FILE_PATH = r"C:\FILES"
+
+A_FILE = 'a.csv'
+D_FILE = 'd.csv'
+T_FILE = 't.csv'
+B_FILE = 'b.csv'
+
+a = pandas.readcsv(os.path.join(FILE_PATH,A_FILE))
+b = pandas.readcsv(os.path.join(FILE_PATH,B_FILE))
+d = pandas.readcsv(os.path.join(FILE_PATH,D_FILE))
+t = pandas.readcsv(os.path.join(FILE_PATH,T_FILE))
 
