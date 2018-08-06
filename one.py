b2ba970537c5bcd5b28922baca13fa2651f60469
bfpapa <42157791+bfpapa@users.noreply.github.com>
2018-08-06 19:43:01 -0400

    Update one.py

--- one.py
+++ one.py
@@ -1,5 +1,6 @@
 import os
-import pandas
+import pandas as pd
+import numpy as np
 
 FILE_PATH = r"C:\FILES"
 
@@ -8,8 +9,15 @@ D_FILE = 'd.csv'
 T_FILE = 't.csv'
 B_FILE = 'b.csv'
 
-a = pandas.readcsv(os.path.join(FILE_PATH,A_FILE))
-b = pandas.readcsv(os.path.join(FILE_PATH,B_FILE))
-d = pandas.readcsv(os.path.join(FILE_PATH,D_FILE))
-t = pandas.readcsv(os.path.join(FILE_PATH,T_FILE))
+A_DF = pd.read_csv(os.path.join(FILE_PATH,A_FILE))
+B_DF = pd.read_csv(os.path.join(FILE_PATH,B_FILE))
+D_DF = pd.read_csv(os.path.join(FILE_PATH,D_FILE))
+T_DF = pd.read_csv(os.path.join(FILE_PATH,T_FILE))
+
+src = {'a': {'file': A_FILE, 'DF': A_DF},
+       'b': {'file': B_FILE, 'DF': B_DF},
+       'd': {'file': D_FILE, 'DF': D_DF},
+       't': {'file': T_FILE, 'DF': T_DF}
+      }
+
 
